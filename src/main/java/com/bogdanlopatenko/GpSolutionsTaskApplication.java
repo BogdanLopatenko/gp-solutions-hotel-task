d6654ee8aa6282928cfb427f4a414bee464f5bb4
Kuleb9lka <winkeylinks@gmail.com>
2026-05-05 15:08:06 +0300

    init commit

---
+++ src/main/java/com/bogdanlopatenko/GpSolutionsTaskApplication.java
@@ -0,0 +1,13 @@
+package com.bogdanlopatenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpSolutionsTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpSolutionsTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GP_solutions_task/GpSolutionsTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GP_solutions_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpSolutionsTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
