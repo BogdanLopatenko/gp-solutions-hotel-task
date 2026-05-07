ce6cc2f0cdf82d3799f01f48c9b078697ec0bf93
Kuleb9lka <winkeylinks@gmail.com>
2026-05-07 19:41:07 +0300

    created histogram

--- src/main/java/com/bogdanlopatenko/repository/HotelRepository.java
+++ src/main/java/com/bogdanlopatenko/repository/HotelRepository.java
@@ -3,8 +3,22 @@ package com.bogdanlopatenko.repository;
 import com.bogdanlopatenko.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long>, JpaSpecificationExecutor<Hotel> {
+
+    @Query("select h.city, count(h) from Hotel h group by h.city")
+    List<Object[]> groupByCity();
+
+    @Query("select h.country, count(h) from Hotel h group by h.country")
+    List<Object[]> groupByCountry();
+
+    @Query("select h.brand.name, count(h) from Hotel h group by h.brand.name")
+    List<Object[]> groupByBrand();
+
+
 }
--- src/main/java/com/bogdanlopatenko/service/HotelService.java
+++ src/main/java/com/bogdanlopatenko/service/HotelService.java
@@ -20,5 +20,4 @@ public interface HotelService {
 
     void addAmenities(Long hotelId, List<AmenityRequestDto> amenities);
 
-
 }
